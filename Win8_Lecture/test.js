b2ec83dc0209284d79a0d601bb24583531f93538
Sohn, ByoungDae <miconblog@gmail.com>
2012-10-29 21:00:58 +0900

    실습 예제

---
+++ Win8_Lecture/test.js
@@ -0,0 +1,32 @@
+﻿var el = $("#msg");
+el.delegate(".box", "click", function () {
+    $(this).hide();
+});
+
+var nCount = 0;
+var nTimer = setInterval(function () {
+    nCount++;
+
+    var elBox = $("<div class='box'>박스1</div>");
+    var R = Math.floor(Math.random() * 10);
+    var G = Math.floor(Math.random() * 10)
+    var B = Math.floor(Math.random() * 10)
+
+    elBox.css({
+        "left": Math.floor(Math.random() * 1000),
+        "top": Math.floor(Math.random() * 500),
+        "background-color": "#" + R + G + B
+    });
+    /*
+    elBox.on("click", function(e){
+        $(this).hide();
+    });*/
+    el.append(elBox);
+
+
+    if (nCount > 100) {
+        clearInterval(nTimer);
+    }
+    console.log(nCount);
+
+}, 100);
