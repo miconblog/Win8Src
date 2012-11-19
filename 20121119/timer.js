433d48334dd23519565949361461abf4c3a61d10
incorp <reflecty@gmail.com>
2012-11-19 15:20:53 +0900

    타이머 스타일 추가

--- 20121119/timer.js
+++ 20121119/timer.js
@@ -1,12 +1,22 @@
-﻿(function (){
+﻿function $(id){
+	return document.getElementById(id);
+}
+
+(function (){
 	var timer = 60;
 	
-	setInterval(function(){
-		console.log(timer);
-		
+	var nT = setInterval(function(){
+		//console.log(timer);
+		//var el = document.getElementById("timer");
+		var el = $("timer");
+		el.innerHTML = "<div>" + timer + "</div>";
 		timer--;
+		
+		if( timer < 0 ){
+			clearInterval(nT);
+			alert("Time Over");
+		}
 	}, 1000);
-
 })();
 
 
