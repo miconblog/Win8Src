f22ba75a79b237080419a1b279b6b282de9bdc45
incorp <reflecty@gmail.com>
2012-11-20 09:20:09 +0900

    풍선 이미지 적용

--- 20121119/game/js/box.js
+++ 20121119/game/js/box.js
@@ -8,12 +8,15 @@
 		
 		elBox.css({
 			"left": Math.floor(Math.random() * 10000) % 750,
-			"top" : Math.floor(Math.random() * 10000) % 350,
-			"background-color": aColor[Math.floor(Math.random() * 100) % 7]
+			"top" : Math.floor(Math.random() * 10000) % 350
+			//"background-color": aColor[Math.floor(Math.random() * 100) % 7]
 		}).fadeIn();
 		
-		
+		elBox[0].onclick = function(){
+			jQuery(this).css("background-position", "-50px 0px");
+		}
 		elCont.appendChild(elBox[0]);
+		
 	}
 	
 	setInterval( createBox, 500)
