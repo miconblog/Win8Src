529d7893209e0b0cb16ff75ce396b58c6dffb93b
incorp <reflecty@gmail.com>
2012-11-20 10:32:00 +0900

    이벤트 버블링 이용한 이벤트 적용

--- 20121119/game/js/box.js
+++ 20121119/game/js/box.js
@@ -1,7 +1,16 @@
 ﻿(function(){
 
+	var elCont = $("#content")[0];
+	elCont.addEventListener("click", function(e){
+		if(e.srcElement.className === "balloon") {
+			
+			console.log("풍선 터트리기", jQuery(e.srcElement).fadeOut() );
+		 
+		}
+	});
+	
 	var createBox = function(){
-		var elCont = $("#content")[0];
+		
 		var elBox = $('<div class="balloon"></div>');
 		var aColor = ["#342353","red","blue","#098343","#afed00","#2343de","#23ffe3"];
 		
@@ -12,11 +21,17 @@
 			"background-position-y": ((Math.floor(Math.random() * 100) % 7) * -50) + "px"
 		}).fadeIn();
 		
-		elBox[0].onclick = function(){
+	/*	elBox[0].onclick = function(){
 			jQuery(this).css("background-position", "-50px 0px");
 		}
-		elCont.appendChild(elBox[0]);
+		*/
 		
+		elCont.appendChild(elBox[0]);
+		/*
+		elBox[0].addEventListener("click", function(){
+			alert(1);
+		});
+			*/
 	}
 	
 	nBoxTimer = setInterval( createBox, 500)
